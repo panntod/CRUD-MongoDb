28cc3c1e3d52a1bc37aed46226b147e05f636685
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-29 20:09:23 +0700

    chore: change structure file

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const { MongoClient } = require("mongodb");
-
-const uri = "mongodb://127.0.0.1:27017";
-const mongoClient = new MongoClient(uri);
-
-async function get_users() {
-  try {
-    await mongoClient.connect();
-
-    const users = await mongoClient
-      .db("crud_mongo")
-      .collection("user")
-      .find({})
-      .toArray();
-
-    console.log(users);
-  } catch (error) {
-    console.error("Error:", error);
-  } finally {
-    await mongoClient.close();
-  }
-}
-
-get_users();
+const userController = require("./controller/user_controller");
+userController.addUser("pandhu", 17);
+userController.getUser();
