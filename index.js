0af86e27d1a0450b0091dd5e102db4b8debad720
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-29 20:23:37 +0700

    chore: use then and catch

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const userController = require("./controller/user_controller");
-userController.addUser("pandhu", 17);
-userController.getUser();
+userController
+  .addUser("pandhu", 17)
+  .then((result) => {
+    console.log("User updated successfully:", result);
+  })
+  .catch((err) => {
+    console.log("Error adding user:", err);
+  });
+
+userController
+  .getUser()
+  .catch((err) => console.log("Cannot get data users \nError:", err));
+
+userController
+  .updateUser("65673792f81fb08fac87fe2b", "asfina", 16)
+  .then((result) => {
+    console.log("User updated successfully:", result);
+  })
+  .catch((error) => {
+    console.error("Error updating user:", error);
+  });
