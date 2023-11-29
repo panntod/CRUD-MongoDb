8938feb7b00ec9d2eaac692f841a4dc968803496
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-30 04:36:32 +0700

    feat: added readline and create new function

--- index.js
+++ index.js
@@ -1,35 +1,65 @@
+const readline = require('readline');
 const userController = require("./controller/user_controller");
-userController
-  .addUser("pandhu", 17)
-  .then((result) => {
-    console.log("User updated successfully:", result);
-  })
-  .catch((err) => {
-    console.log("Error adding user:", err);
-  });
 
-userController
-  .getUser()
-  .catch((err) => console.log("Cannot get data users \nError:", err));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-userController
-  .updateUser("65673792f81fb08fac87fe2b", "asfina", 16)
-  .then((result) => {
-    console.log("User updated successfully:", result);
-  })
-  .catch((error) => {
-    console.error("Error updating user:", error);
-  });
+function executeCommand(command) {
+  switch (command) {
+    case 'addUser':
+      userController
+        .addUser("pandhu", 17)
+        .then((result) => {
+          console.log("User added successfully:", result);
+          rl.close();
+        })
+        .catch((err) => {
+          console.log("Error adding user:", err);
+          rl.close();
+        });
+      break;
+    case 'getUser':
+      userController
+        .getUser()
+        .catch((err) => console.log("Cannot get data users \nError:", err))
+        .finally(() => rl.close());
+      break;
+    case 'updateUser':
+      userController
+        .updateUser("65673792f81fb08fac87fe2b", "asfina", 16)
+        .then((result) => {
+          console.log("User updated successfully:", result);
+          rl.close();
+        })
+        .catch((error) => {
+          console.error("Error updating user:", error);
+          rl.close();
+        });
+      break;
+    case 'deleteUser':
+      userController
+        .deleteUser("65673792f81fb08fac87fe2b")
+        .then((result) => {
+          if (result.deletedCount === 1) {
+            console.log("User deleted successfully");
+          } else {
+            console.log("User not found or already deleted");
+          }
+          rl.close();
+        })
+        .catch((error) => {
+          console.error("Error deleting user:", error);
+          rl.close();
+        });
+      break;
+    default:
+      console.log('Invalid command');
+      rl.close();
+  }
+}
 
-userController
-  .deleteUser("65673792f81fb08fac87fe2b")
-  .then((result) => {
-    if (result.deletedCount === 1) {
-      console.log("User deleted successfully");
-    } else {
-      console.log("User not found or already deleted");
-    }
-  })
-  .catch((error) => {
-    console.error("Error deleting user:", error);
-  });
+rl.question('Enter command (addUser, getUser, updateUser, deleteUser): ', (command) => {
+  executeCommand(command);
+});
